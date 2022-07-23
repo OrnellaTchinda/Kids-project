34ce3cadc9f079c8a3bd2076b690cbaf87642d03
Ornella Tchinda <ornella.tchinda@yahoo.fr>
2022-07-22 22:09:28 -0400

    Working kids show api

---
+++ server.js
@@ -0,0 +1,64 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const shows = {
+
+    'cars 2': {
+        'type': "Movies",
+        'streamingService': 'Disney+',
+        'star': 'Lightning McQueen',
+        'likes': ''
+    },
+
+    'cars': {
+        'type': "Movies",
+        'streamingService': 'Disney+',
+        'star': 'Lightning McQueen',
+        'likes': ''
+    },
+
+    'masha and mishka': {
+        'type': "series",
+        'streamingService': 'Netflix',
+        'star': 'Masha, Mishka',
+        'likes': ''
+    },
+
+    'unknown': {
+        'type': "unknown",
+        'streamingService': 'unknown',
+        'star': 'unknown',
+        'likes': ''
+    }
+
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+});
+
+// app.get('/api', (request, response) => {
+//     response.json(shows)
+// })
+
+
+
+app.get('/api/:name', (request, response) => {
+    const showsName = request.params.name.toLowerCase()
+    console.log(shows[showsName])
+
+    if (shows[showsName]) {
+        response.json(shows[showsName])
+    } else {
+        response.json(shows['unknown'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}`)
+})
+
+
+
